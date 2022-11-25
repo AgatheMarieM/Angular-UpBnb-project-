aeeecfea3719c6589d021519162d69ea411cc9b5
Agathe Marie M <94805574+AgatheMarieM@users.noreply.github.com>
2022-11-25 12:37:21 +0000

    const Base_URL must be above @Injectable getHouses() function: must specify get<HousesData> to define the structure we want to receive

--- src/app/up-bnb.service.ts
+++ src/app/up-bnb.service.ts
@@ -1,17 +1,19 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
+import {HousesData} from "./interfaces";
+
+const BASE_URL = "https://m9-frontend.upskill.appx.pt/upbnb";
 
 @Injectable({
   providedIn: 'root'
 })
 
-const BASE_URL = "https://m9-frontend.upskill.appx.pt/upbnb";
 
 export class UpBnbService {
 
-  constructor(private http: HttpClient) {}
+  constructor(public http: HttpClient) {}
 
   getHouses(){
-    return this.http.get(BASE_URL + "/casas?page=1&limit=8");
+    return this.http.get<HousesData>(BASE_URL + "/casas");
   }
 }
