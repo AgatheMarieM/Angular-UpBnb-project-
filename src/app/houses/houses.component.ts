168846f557db934d96f62ae5288f0db67f6a555b
Agathe Marie M <94805574+AgatheMarieM@users.noreply.github.com>
2022-11-25 12:27:53 +0000

    hhtp requested created inside the component

--- src/app/houses/houses.component.ts
+++ src/app/houses/houses.component.ts
@@ -1,17 +1,25 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {UpBnbService} from "../up-bnb.service";
-import {House} from "../interfaces";
+import {House, HousesData} from "../interfaces";
 
 @Component({
   selector: 'app-houses',
   templateUrl: './houses.component.html',
   styleUrls: ['./houses.component.scss']
 })
+
 export class HousesComponent {
-  constructor(private upBnb: UpBnbService ) {
+  constructor(private upBnbService: UpBnbService) {
   }
 
- houses?: House[];
+  houses?: House[] = []; //list of houses is at first an empty list
+
+  ngOnInit(): void {
+    this.upBnbService.getHouses().subscribe((houses: HousesData) => {
+      this.houses = houses.data; //the object is a list of houses
+      console.log(this.houses);
+    })
 
 
+  }
 }
