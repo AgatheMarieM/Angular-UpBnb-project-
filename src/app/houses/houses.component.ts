28da2233eafe3c623f1d0a96ec46d076d025d5c1
Agathe Marie M <94805574+AgatheMarieM@users.noreply.github.com>
2022-11-25 10:47:21 +0000

    imported {House} interface to houses.component.ts, this component will display all houses from API

--- src/app/houses/houses.component.ts
+++ src/app/houses/houses.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import {UpBnbService} from "../up-bnb.service";
+import {House} from "../interfaces";
 
 @Component({
   selector: 'app-houses',
@@ -9,6 +10,8 @@ import {UpBnbService} from "../up-bnb.service";
 export class HousesComponent {
   constructor(private upBnb: UpBnbService ) {
   }
+
+ houses?: House[];
 
 
 }
