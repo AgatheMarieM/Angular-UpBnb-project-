421acba9f05e414933652af93eedac2540793176
Agathe Marie M <94805574+AgatheMarieM@users.noreply.github.com>
2022-11-25 10:45:33 +0000

    created {House} interface to implement house structure

---
+++ src/app/interfaces.ts
@@ -0,0 +1,12 @@
+interface House  {
+  id: number,
+  title: string,
+  country: string,
+  city: string,
+  rating: number,
+  price: number,
+  host_type: string,
+  featured_photo: string
+}
+
+export {House};
