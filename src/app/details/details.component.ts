efe498dd5c0ac47801feeeca39e8501c35829d68
Agathe Marie M <94805574+AgatheMarieM@users.noreply.github.com>
2022-11-26 17:45:16 +0000

    imported {ActivatedRoute}, {UpBnbService} and {HouseDetails} to be able to show each house details. In constructor, this.id_house = route.snapshot.params['id_house']; refers to the selected house details

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
+import {UpBnbService} from "../up-bnb.service";
+import {HouseDetails} from "../interfaces";
 
 @Component({
   selector: 'app-details',
@@ -6,5 +9,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./details.component.scss']
 })
 export class DetailsComponent {
+
+  id_house: number;
+  details?: HouseDetails //| null = null;
+
+
+  constructor(private route: ActivatedRoute, public UpBnbService: UpBnbService) {
+    this.id_house = route.snapshot.params['id_house']; //this.id: referencia à uma propriedade dele proprio
+    //this.detalhes = livrariaService.getDetalhes(this.id);
+  }
+
+
+
 
 }
