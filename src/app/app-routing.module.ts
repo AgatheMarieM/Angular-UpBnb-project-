93a2370bdabe4d39fadd3bff9ef820f0c790ce68
Agathe Marie M <94805574+AgatheMarieM@users.noreply.github.com>
2022-11-26 17:42:29 +0000

    Defined details component route path in app-routing.module.ts. We are passing the house id in the URL/path, to get specifically to that house details: {path: 'details/:id_house', component: DetailsComponent}

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import {RouterModule, Routes} from "@angular/router";
 import {HousesComponent} from "./houses/houses.component";
+import {DetailsComponent} from "./details/details.component";
 
 
 const routes: Routes = [
-  {path: '', redirectTo: '/livros', pathMatch: 'full'},
+  {path: '', redirectTo: '/houses', pathMatch: 'full'}, //default path
   {path: 'houses', component: HousesComponent},
-  // {path: 'details/:id_house', component: DetailsComponent},
+  {path: 'details/:id_house', component: DetailsComponent}
 ];
 
 @NgModule({
