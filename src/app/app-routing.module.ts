bd841bad0e49902f7c4b8acc957c728b1b88af8b
Agathe Marie M <94805574+AgatheMarieM@users.noreply.github.com>
2022-11-27 13:36:07 +0000

    "id_house" rename as "id"

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import {RouterModule, Routes} from "@angular/router";
 import {HousesComponent} from "./houses/houses.component";
 import {DetailsComponent} from "./details/details.component";
+import {HouseComponent} from "./house/house.component";
 
 
 const routes: Routes = [
   {path: '', redirectTo: '/houses', pathMatch: 'full'}, //default path
   {path: 'houses', component: HousesComponent},
-  {path: 'details/:id_house', component: DetailsComponent}
+  {path: 'house', component: HouseComponent},
+  {path: 'details/:id', component: DetailsComponent}
 ];
 
 @NgModule({
