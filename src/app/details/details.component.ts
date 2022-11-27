16a0c8a91009149e35f598547937fcc3d7011a9c
Agathe Marie M <94805574+AgatheMarieM@users.noreply.github.com>
2022-11-27 13:33:35 +0000

    renamed id_house to id

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -1,25 +1,30 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {ActivatedRoute} from "@angular/router";
 import {UpBnbService} from "../up-bnb.service";
 import {HouseDetails} from "../interfaces";
 
 @Component({
   selector: 'app-details',
+
   templateUrl: './details.component.html',
   styleUrls: ['./details.component.scss']
 })
 export class DetailsComponent {
 
-  id_house: number;
-  details?: HouseDetails //| null = null;
+  id: number;
+  details?: HouseDetails;
 
 
   constructor(private route: ActivatedRoute, public UpBnbService: UpBnbService) {
-    this.id_house = route.snapshot.params['id_house']; //this.id: referencia à uma propriedade dele proprio
-    //this.detalhes = livrariaService.getDetalhes(this.id);
+    this.id = route.snapshot.params['id']; //this.id: referencia à uma propriedade dele proprio
   }
 
-
+  ngOnInit(): void {
+    this.UpBnbService.getDetails(this.id).subscribe((details) => {
+        this.details = details;
+      }
+    )
+  }
 
 
 }
