e0860a6c0a2be0df8a851a611c233f107d61df07
Agathe Marie M <94805574+AgatheMarieM@users.noreply.github.com>
2022-11-28 22:33:17 +0000

    created 3 components: Welcome, Favorites and SearchBar components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,16 @@ import {RouterModule, Routes} from "@angular/router";
 import {HousesComponent} from "./houses/houses.component";
 import {DetailsComponent} from "./details/details.component";
 import {HouseComponent} from "./house/house.component";
+import {WelcomeComponent} from "./welcome/welcome.component";
+import {FavoritesComponent} from "./favorites/favorites.component";
 
 
 const routes: Routes = [
-  {path: '', redirectTo: '/houses', pathMatch: 'full'}, //default path
+  {path: '', redirectTo: '/welcome', pathMatch: 'full'}, //default path
   {path: 'houses', component: HousesComponent},
   {path: 'house', component: HouseComponent},
+  {path: 'welcome', component: WelcomeComponent},
+  {path: 'favorites', component: FavoritesComponent},
   {path: 'details/:id', component: DetailsComponent}
 ];
 
