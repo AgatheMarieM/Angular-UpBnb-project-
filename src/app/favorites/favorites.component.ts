4ee907bd56e3e73c7b37cba227ad99ceeed5479f
Agathe Marie M <94805574+AgatheMarieM@users.noreply.github.com>
2022-12-01 19:18:49 +0000

    calling getFavoriteHouse() from the service to get an object with favorite houses We loop through this object and push each house country into an array called favHousesByCountry (if they are not already there)

--- src/app/favorites/favorites.component.ts
+++ src/app/favorites/favorites.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {UpBnbService} from "../up-bnb.service";
+import {House, HousesData} from "../interfaces";
+
 
 @Component({
   selector: 'app-favorites',
@@ -7,6 +9,42 @@ import {UpBnbService} from "../up-bnb.service";
   styleUrls: ['./favorites.component.scss']
 })
 export class FavoritesComponent {
-  constructor(private upBnbService: UpBnbService) {
+
+  constructor(public upBnbService: UpBnbService) {
+  }
+
+  favHouses?: House[] = []; //use interface house, empty array at first
+  favHousesByCountry?: {
+    [index: string]: House[]
+  } = {};
+
+  // keysArray?: string[] = [];
+
+
+  ngOnInit(): void {
+    //get houses with these ids
+    this.upBnbService.getFavoriteHouses().subscribe((houses: HousesData) => {
+      this.favHouses = houses.data; //the object is a list of houses
+
+
+      this.favHouses.forEach((favHouse) => {
+          if (this.favHousesByCountry) {
+            if (!this.favHousesByCountry[favHouse.country]) {
+              this.favHousesByCountry[favHouse.country] = [];
+              this.favHousesByCountry[favHouse.country].push(favHouse)
+            } else {
+              this.favHousesByCountry[favHouse.country].push(favHouse)
+            }
+          }
+        }
+      )
+      console.log(this.favHousesByCountry);
+      //@ts-ignore
+
+/*
+      console.log(Object.keys(this.favHousesByCountry));
+*/
+    })
+
   }
 }
