42b4b65c90b4e574757f2921141ae687be90a378
Agathe Marie M <94805574+AgatheMarieM@users.noreply.github.com>
2022-12-01 19:21:02 +0000

    created getFavoriteHouses() and reorganized code

--- src/app/up-bnb.service.ts
+++ src/app/up-bnb.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {HouseDetails, HouseFeatures, HouseHost, HousePhotos, HouseReviews, HousesData} from "./interfaces";
 
@@ -12,57 +12,65 @@ const BASE_URL = "https://m9-frontend.upskill.appx.pt/upbnb";
 
 export class UpBnbService {
 
-  constructor(public http: HttpClient) {}
+  constructor(public http: HttpClient) {
+  }
 
-  getSearchResult(input: string){
+  getSearchResult(input: string) {
     return this.http.get<HousesData>(BASE_URL + "/casas?search=" + input);
   }
 
-  getHouses(){
+  getHouses() {
     return this.http.get<HousesData>(BASE_URL + "/casas");
   }
 
-  getDetails(id: number){
-    return this.http.get<HouseDetails>(BASE_URL + "/casas/" + id ); //take off the  ":" from Postman
-  }
-  //HouseDetails is not a list! <HouseDetails[]> not needed, no "casting"!
-
-  getFeatures(id: number){
-    return this.http.get<HouseFeatures>(BASE_URL + "/casas/" + id + "/features");
-  }
-
-  getHouseHost(id: number){
-    return this.http.get<HouseHost>(BASE_URL + "/casas/" + id + "/host");
-  }
-
-  getHousePhotos(id: number){
-    return this.http.get<HousePhotos>(BASE_URL + "/casas/" + id + "/photos");
-  }
-
-  getHouseReviews(id: number){
-    return this.http.get<HouseReviews>(BASE_URL + "/casas/" + id + "/reviews");
-  }
-
-//Managing favorites:
+  //Managing favorites:
   favorites: number[] = JSON.parse(localStorage.getItem("favorites") || "[]"); //get favorites list or empty list
 
-  isFavorite(id:number){
+  isFavorite(id: number) {
     return this.favorites.includes(id);
   }
 
-  toggleFavorite(id: number){
-    if(this.isFavorite(id)){
+  toggleFavorite(id: number) {
+    if (this.isFavorite(id)) {
       //remove id from fav (was in the list and user clicks again > removing)
       this.favorites.splice(this.favorites.indexOf(id), 1) //favorite was in the list, deleting it
-    } else{
+    } else {
       this.favorites.push(id); //we push it into the list, wasn't fav yet
     }
     localStorage.setItem("favorites", JSON.stringify(this.favorites));//setting favorites item in the localStorage
   }
 
-  getFavorites(){
+  getFavorites() {
     return this.favorites;
   }
+
+  getFavoriteHouses() {
+    return this.http.get<HousesData>(BASE_URL + "/casas?ids=" + this.favorites.join());
+  }
+
+
+  getDetails(id: number) {
+    return this.http.get<HouseDetails>(BASE_URL + "/casas/" + id); //take off the  ":" from Postman
+  }
+
+  //HouseDetails is not a list! <HouseDetails[]> not needed, no "casting"!
+
+  getFeatures(id: number) {
+    return this.http.get<HouseFeatures>(BASE_URL + "/casas/" + id + "/features");
+  }
+
+  getHouseHost(id: number) {
+    return this.http.get<HouseHost>(BASE_URL + "/casas/" + id + "/host");
+  }
+
+  getHousePhotos(id: number) {
+    return this.http.get<HousePhotos>(BASE_URL + "/casas/" + id + "/photos");
+  }
+
+  getHouseReviews(id: number) {
+    return this.http.get<HouseReviews>(BASE_URL + "/casas/" + id + "/reviews");
+  }
+
 
 
 
