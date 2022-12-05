a88586037a6049decd65dff4f15b9c07b92d7b98
Agathe Marie M <94805574+AgatheMarieM@users.noreply.github.com>
2022-12-05 18:51:16 +0000

    erased deprecated input noite?

--- src/app/house/house.component.ts
+++ src/app/house/house.component.ts
@@ -25,7 +25,6 @@ export class HouseComponent {
   @Input() host!: string;
   @Input() price!: number;
   @Input() stars?: number;
-  @Input() noite?: string;
   @Input() type?: string;
   @Input() time?: string;
 
