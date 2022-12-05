ad4a9de0b1a748beb23dafaf13417355c1582e16
Agathe Marie M <94805574+AgatheMarieM@users.noreply.github.com>
2022-12-05 18:37:03 +0000

    refactored search bar : -on initialization, input is '' and i = 1 (i is the page index) API sends 8 first houses and populate empty array this.houses -getInput() receives input from user and sets this.houses to empty array again before pushing results into this.houses and showing results - getMoreResults() increments i and push more results into this.houses array

--- src/app/houses/houses.component.ts
+++ src/app/houses/houses.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {UpBnbService} from "../up-bnb.service";
 import {House, HousesData} from "../interfaces";
 
@@ -8,18 +8,44 @@ import {House, HousesData} from "../interfaces";
   styleUrls: ['./houses.component.scss']
 })
 
-export class HousesComponent {
+export class HousesComponent implements OnInit {
   constructor(private upBnbService: UpBnbService) {
   }
 
-  noite?: string = "noite";
-  houses?: House[] = []; //list of houses is at first an empty list
+  input: string = '';
+  i: number = 1; //refers to page number
+  houses: House[] = []; //list of houses is at first an empty list
 
   ngOnInit(): void {
-    this.upBnbService.getHouses().subscribe((houses: HousesData) => {
-      this.houses = houses.data; //the object is a list of houses
-    })
-
-
+    this.getResults()
   }
+
+  getInput(value: string) {
+    this.i = 1;
+    this.input = value;
+    this.getResults()
+  }
+
+  getMoreResults() {
+    this.i++;
+    this.getResults();
+  }
+
+  getResults() {
+    this.upBnbService.getSearchResult(this.input, this.i).subscribe((houses: HousesData) => {
+      if (this.input == '') {
+        this.houses = houses.data;
+      } else if (this.input != '') {
+        if (this.i > 1) {
+          this.houses.push(...houses.data);
+        } else {
+          this.houses = [];
+          this.houses.push(...houses.data);
+        }
+      }
+    })
+  }
+  //on initialization, input is '' and i = 1, API sends 8 first houses and populate empty array this.houses
+  //getInput(): receives input from user, sets this.houses to empty array again, and pushes fresh results into this.houses
+  //getMoreResults() increments i, pushes more results from the same input into this.houses array
 }
